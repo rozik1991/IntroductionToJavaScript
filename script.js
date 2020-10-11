64fb85df942db340966e4ea4fa66f912e91acd52
rozik1991 <allamuratoglu@gmail.com>
2020-10-11 06:30:29 +0500

    Add index readme script style files

---
+++ script.js
@@ -0,0 +1,12 @@
+let myAge = 29; //This is my age declared in number. 
+let earlyYears = 2; //This variable can be changed.
+earlyYears = earlyYears * 10.5; //In an other way earlyYears *= 10.5;
+laterYears = myAge - 2; //This variable can be changed.
+laterYears = laterYears * 4; //number of dog years 
+console.log(earlyYears); //Checking piont of earlyYears.
+console.log(laterYears); //Checking point of laterYears.
+let myAgeInDogYears = earlyYears + laterYears; //My age represented by dog age.
+let myName = 'RoziK'; 
+console.log(myName.toLowerCase()); //This action will write my name only lowercase.
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} 
+old in dog years.`); //This is for task number 9 (string interpolation).
